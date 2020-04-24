61ada51f8950e7ee3f19c12367cdf988aebcf8dc
matmcf <63942762+matmcf@users.noreply.github.com>
2020-04-25 09:01:07 +1200

    Update vegeshop_custom.js
    Hide pick up time

--- vegeshop_custom.js
+++ vegeshop_custom.js
@@ -3,15 +3,15 @@ ec.order.extraFields.how_did_you_find_us = {
     'title': 'How did you find us?',
     'type': 'select',
     'required': false,
-    'selectOptions': ['Google Ads', 'Friend told me', 'TV show', 'Other'],
-    'value': 'TV show', // Default value
+    'selectOptions': ['Facebook', 'Friend', 'Other'],
+    'value': 'Friend', // Default value
     'checkoutDisplaySection': 'payment_details'
 };
 
 Ecwid.refreshConfig();
 
 // Add pickup time selection for customer
-ec.order.extraFields.ecwid_pickup_time = {
+/*ec.order.extraFields.ecwid_pickup_time = {
     'title': '_msg_ShippingDetails.pickup.customer_header',
     'required': true,
     'type': 'datetime',
@@ -19,7 +19,7 @@ ec.order.extraFields.ecwid_pickup_time = {
     'orderDetailsDisplaySection': 'payment_details',
 }
 
-Ecwid.refreshConfig();
+Ecwid.refreshConfig(); */
 
 // Hidden field, which is not shown at checkout
 ec.order.extraFields.my_custom_field = {
